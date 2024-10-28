0afcb2b4f7d3e508d895ea7418448d2ac65fb1a3
观澜听雨 <tangxiaochuane@yeah.net>
2024-10-28 10:36:52 +0800

    authentication setting

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <main className=' min-h-screen '>{children}</main>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='relative'>
-      <Button
-        size={"xl"}
-        variant={"custom"}
-      >
-        <Image
-          src={"/svgs/dog-breed-svgrepo-com.svg"}
-          alt='dog'
-          height={45}
-          width={45}
-        />
-      </Button>
+    <div className='flex flex-col h-full min-h-screen items-center justify-center bg-gradient-to-tr from-sky-400 to-blue-400'>
+      <div className=' space-y-6 text-center'>
+        <h1 className='relative flex items-baseline justify-center gap-2 text-white text-6xl font-semibold drop-shadow-md'>
+          <Image
+            src={"/svgs/locked-svgrepo-com.svg"}
+            alt=''
+            width={50}
+            height={50}
+          />
+          Auth
+        </h1>
+        <p className=' text-white text-lg'>
+          A simple authenticatin service
+        </p>
+        <div className=''>
+          <Button
+            variant={"secondary"}
+            size={"lg"}
+          >
+            Sign in
+          </Button>
+        </div>
+      </div>
     </div>
   );
+  // return (
+  //   <div className='relative'>
+  //     <Button
+  //       size={"xl"}
+  //       variant={"custom"}
+  //     >
+  //       <Image
+  //         src={"/svgs/dog-breed-svgrepo-com.svg"}
+  //         alt='dog'
+  //         height={45}
+  //         width={45}
+  //       />
+  //     </Button>
+  //   </div>
+  // );
 }
