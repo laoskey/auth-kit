57dea7fd570592dc34892b214f952f082c8c6d0e
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-03 22:09:11 +0800

    implement the getPasswordTokenFunc

---
+++ data/password-reset-token.ts
@@ -0,0 +1,24 @@
+import { db } from "@/lib/db";
+
+export const getPasswordTokenByToken = async (token: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findUnique({
+      where: { token },
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
+export const getPasswordTokenByEmail = async (email: string) => {
+  try {
+    const passwordResetToken = await db.passwordResetToken.findFirst({
+      where: { email },
+    });
+
+    return passwordResetToken;
+  } catch {
+    return null;
+  }
+};
