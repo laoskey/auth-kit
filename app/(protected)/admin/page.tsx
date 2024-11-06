dd4b1f1524d9ef94875b8ee067b594d0a4cd54a5
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-06 18:11:11 +0800

    implement the Admin route content page

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { RoleGate } from "@/components/auth/RoleGate";
+import { FormSuccess } from "@/components/FormSuccess";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
+
+export default function AdminPage() {
+  return (
+    <Card className='w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>🔑 Admin</p>
+      </CardHeader>
+      <CardContent className='space-y-4'>
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message='Your are allowed to see this content' />
+        </RoleGate>
+        <div
+          className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md
+         '
+        >
+          <p className='text-sm font-medium'>Admin-only API Route</p>
+
+          <Button>Click to test</Button>
+        </div>
+        <div
+          className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md
+         '
+        >
+          <p className='text-sm font-medium'>
+            Admin-only Server Action
+          </p>
+
+          <Button>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
